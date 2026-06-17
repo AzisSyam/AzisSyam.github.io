037a59a7d5660487740bf3ff815d9edfebfa5409
AzisSyam <abdazissyam99@gmail.com>
2026-06-17 23:30:52 +0800

    feat: init portfolio web Astro project

---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'Plus Jakarta Sans', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          bg: '#384959',
+          primary: '#BDDDFC',
+        },
+        black: '#000000',
+        white: '#FFFFFF',
+        gray: {
+          50: '#FAFAFA',
+          100: '#F5F5F5',
+          200: '#EEEEEE',
+          300: '#E0E0E0',
+          400: '#BDBDBD',
+          500: '#9E9E9E',
+          600: '#757575',
+          700: '#616161',
+          800: '#424242',
+          900: '#212121',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
